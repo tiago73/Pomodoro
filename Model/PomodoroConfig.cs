6e63619ca638624a9c4a876cf3a8a9f3284413f5
tiago73 <tiagodsf73@gmail.com>
2020-04-22 01:13:33 -0300

    Commit

---
+++ Model/PomodoroConfig.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Pomodoro.Model
+{
+    
+    public class PomodoroConfig
+    {
+        [Required(ErrorMessage ="*")]
+        [Range(1,60, ErrorMessage = "Tempo de duração de Pomodoro inválido. o valor o deve ser entre 1 e 60")]
+        public int PomodoroDuration { get; set; }
+        [Range(1, 15, ErrorMessage = "Tempo de duração de Pomodoro inválido. o valor o deve ser entre 1 e 15")]
+        public int PausaCurta { get; set; }
+        [Range(15, 60, ErrorMessage = "Tempo de duração de Pomodoro inválido. o valor o deve ser entre 15 e 60")]
+        public int PausaLonga { get; set; }
+    }
+    public enum TimerType
+    {
+        Pomodro = 1,
+        PausaCurta = 2,
+        PausaLonga = 3
+    }
+}
